a7cc37cea8d2abdaf354ea13639a7510d733f4cc
s <chunwooSv@gmail.com>
2017-08-24 18:03:11 +0900

    2017.08.24(수업 참고용)

--- src/js/admin/activity-edit.js
+++ src/js/admin/activity-edit.js
@@ -43,7 +43,7 @@ function requestTheres(groupId) {
             groupId: groupId
         },
         success: function(result) {
-            var thereItemsTemplate = require('../../template/admin/there-items.hbs');
+            var thereItemsTemplate = require('../../template/admin/there-itmes.hbs');
             var thereItemsHtml = thereItemsTemplate(result);
 
             $('#hta-there-select .dropdown-menu').html(thereItemsHtml);
@@ -54,6 +54,37 @@ function requestTheres(groupId) {
                 var thereId = $(this).attr('there-id');
                 model.thereId = thereId;
             });
+        }
+    });
+}
+
+function addPreview(url, saved) {
+    var preview = $('<li><div class="hta-photo-remove">X</div></li>');
+    preview.attr('saved', saved);
+
+    preview.css({
+        'background-image': 'url(' + url + ')',
+        'width': '50px',
+        'height': '50px'
+    });
+
+    $('.hta-photos').append(preview);
+
+    $('.hta-photos > li:last-child .hta-photo-remove').on('click', function() {
+        var photo = $(this).parent('li');
+
+        var saved = photo.attr('saved') === 'true';
+        var savedPhotoCount = model.photos ? model.photos.length : 0
+
+        var index = saved ? photo.index() : photo.index() - savedPhotoCount;
+
+        if (saved) {
+            model.photos[index] = '_removed_';
+            photo.hide();
+        }
+        else {
+            photos.splice(index, 1);
+            photo.remove();
         }
     });
 }
@@ -75,15 +106,7 @@ $('#hta-activity-photos').on('change', function() {
         var fileReader = new FileReader();
 
         fileReader.onload = function(event) {
-            var preview = $('<li></li>');
-
-            preview.css({
-                'background-image': 'url(' + event.target.result + ')',
-                'width': '50px',
-                'height': '50px'
-            });
-
-            $('.hta-photos').append(preview);
+            addPreview(event.target.result, false);
         };
 
         fileReader.readAsDataURL(file);
@@ -94,36 +117,38 @@ $('#hta-activity-id').on('change', function() {
     validId = false;
 });
 
-$('.hta-check-duplicate').on('click', function() {
-    var id = $('#hta-activity-id').val().trim();
+function addCheckDuplicateEvent() {
+    $('.hta-check-duplicate').on('click', function() {
+        var id = $('#hta-activity-id').val().trim();
 
-    if (!id) {
-        alert('액티비티ID를 입력하세요.');
-        $('#hta-activity-id').focus();
-        return;
-    }
-    else if (!/^[a-zA-Z][0-9a-zA-Z\-]{3,}$/.test(id)) {
-        alert('잘못된 ID형식입니다.');
-        $('#hta-activity-id').focus();
-        return;
-    }
-
-    $.ajax({
-        url: '/api/admin/activity/' + id,
-        method: 'OPTIONS',
-        success: function(result) {
-            if (!result.exists) {
-                alert('사용할 수 있는 ID입니다.');
-                validId = true;
-            }
-            else {
-                alert('사용할 수 없는 ID입니다.');
-                $('#hta-activity-id').focus();
-                validId = false;
-            }
+        if (!id) {
+            alert('액티비티ID를 입력하세요.');
+            $('#hta-activity-id').focus();
+            return;
         }
+        else if (!/^[a-zA-Z][0-9a-zA-Z\-]{3,}$/.test(id)) {
+            alert('잘못된 ID형식입니다.');
+            $('#hta-activity-id').focus();
+            return;
+        }
+
+        $.ajax({
+            url: '/api/admin/activity/' + id,
+            method: 'OPTIONS',
+            success: function(result) {
+                if (!result.exists) {
+                    alert('사용할 수 있는 ID입니다.');
+                    validId = true;
+                }
+                else {
+                    alert('사용할 수 없는 ID입니다.');
+                    $('#hta-activity-id').focus();
+                    validId = false;
+                }
+            }
+        });
     });
-});
+}
 
 $('.hta-activity-info-list .hta-add-row').on('click', function() {
     model.lists.push({
@@ -228,5 +253,138 @@ function addBtnRowEvents() {
         row.replaceWith(html);
 
         addInfoListsEvents();
+    });
+}
+
+$('.hta-save').on('click', function() {
+    model.id = $('#hta-activity-id').val().trim();
+    model.name = $('#hta-activity-name').val().trim();
+    model.video = $('#hta-activity-video').val().trim();
+    model.location = {
+        lat: $('#hta-activity-lat').val().trim(),
+        lng: $('#hta-activity-lng').val().trim()
+    };
+    model.intro = $('#hta-activity-intro').val().trim();
+
+    if (!model.thereId) {
+        alert('지역을 선택하세요.');
+        return;
+    }
+    else if (!model.id) {
+        alert('액티비티ID를 입력하세요.');
+        $('#hta-activity-id').focus();
+        return;
+    }
+    else if (!validId) {
+        alert('액티비티ID 중복확인을 해주세요.');
+        $('#hta-activity-id').focus();
+        return;
+    }
+    else if (!model.name) {
+        alert('액티비티명을 입력하세요.');
+        $('#hta-activity-name').focus();
+        return;
+    }
+    else if (!photos.length && !_.filter(model.photos, function(value) {
+            return value !== '_removed_';
+        }).length) {
+        alert('사진을 한개 이상 추가하세요.');
+        return;
+    }
+    else if (!model.location.lat) {
+        alert('위도를 입력하세요.');
+        $('#hta-activity-lat').focus();
+        return;
+    }
+    else if (!model.location.lng) {
+        alert('경도를 입력하세요.');
+        $('#hta-activity-lng').focus();
+        return;
+    }
+    else if (!model.intro) {
+        alert('소개를 입력하세요.');
+        $('#hta-activity-intro').focus();
+        return;
+    }
+
+    var formData = new FormData();
+    formData.append('model', JSON.stringify(model));
+
+    photos.forEach(function(photo) {
+        formData.append('photos', photo);
+    });
+
+    $.ajax({
+        url: '/api/admin/activity/' + model.id,
+        method: 'POST',
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function(result) {
+            alert('정상적으로 저장되었습니다.');
+
+            location.href = './activity-edit.html?id=' + model.id;
+        },
+        error: function() {
+            alert('저장 중 오류가 발생하였습니다.');
+        }
+    });
+});
+
+$('.hta-cancel').on('click', function() {
+    history.back();
+});
+
+function addDeleteEvent() {
+    $('.hta-delete').on('click', function() {
+        // TODO 삭제처리
+    });
+}
+
+function init() {
+    if (model.id) {
+        $('#hta-there-group-select button').attr('disabled', true);
+        $('#hta-there-select button').attr('disabled', true);
+        $('#hta-there-select .dropdown-title').text(model.thereId);
+
+        $('#hta-activity-id').val(model.id);
+        $('#hta-activity-id').attr('disabled', true);
+        $('.hta-check-duplicate').hide();
+        validId = true;
+
+        addDeleteEvent();
+    }
+    else {
+        $('.hta-delete').hide();
+
+        addCheckDuplicateEvent();
+    }
+
+    $('#hta-activity-name').val(model.name);
+
+    if (model.photos) {
+        model.photos.forEach(function(url) {
+            addPreview(url, true);
+        });
+    }
+
+    $('#hta-activity-video').val(model.video);
+    $('#hta-activity-lat').val(model.location.lat);
+    $('#hta-activity-lng').val(model.location.lng);
+    $('#hta-activity-intro').val(model.intro);
+
+    setInfoLists();
+}
+
+if (!params.get('id')) {
+    init();
+}
+else {
+    $.ajax({
+        url: '/api/activity/' + params.get('id'),
+        success: function(result) {
+            model = result;
+            init();
+        }
     });
 }
