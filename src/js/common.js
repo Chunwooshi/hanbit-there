6816acd16c1fca7ac7f280a312e64cab5a16de31
s <chunwooSv@gmail.com>
2017-08-31 17:51:32 +0900

    2017.08.31(수업 참고용)

--- src/js/common.js
+++ src/js/common.js
@@ -42,14 +42,14 @@ $('.header-logo').on('click', function() {
 });
 
 function attachEvents() {
-    $('.h-sub-m > li').on('click', function() {
+    $('.header-sub-menu > li').on('click', function() {
         var theresId = $(this).attr('theres-id');
 
         location.href = './theres.html?id=' + theresId;
     });
 
     $('.header-menu > li').on('mouseover', function() {
-        var subMenu = $(this).find('.h-sub-m');
+        var subMenu = $(this).find('.header-sub-menu');
         var subMenuItemWidth = subMenu.find('li').outerWidth();
         var subMenuItemMaxCount = 5;
         var subMenuItemCount = subMenu.find('li').length;
@@ -70,39 +70,149 @@ function attachEvents() {
     });
 
     $('.header-menu > li').on('mouseout', function() {
-        $(this).find('.h-sub-m').hide();
+        $(this).find('.header-sub-menu').hide();
     });
 }
+
 $('.header-btn-member').on('click', function() {
+    $.ajax({
+        url: '/api/member/get',
+        success: function(result) {
+            openMemberLayer(result);
+        }
+    });
+});
+
+function openMemberLayer(memberInfo) {
     $('body').append('<div class="overlay-layer dark-layer"></div>');
     $('body').css('overflow', 'hidden');
 
-    var memberLayer = require('../template/member-layer2.hbs');
+    var memberLayerTemplate = require('../template/member-layer2.hbs');
+    var memberLayer = memberLayerTemplate(memberInfo);
 
     $('body').append(memberLayer);
-
-    $('.ht-member-toggle').on('click', function() {
-        $('.ht-sign-in').toggle();
-        $('.ht-sign-up').toggle();
-    });
 
     $('.ht-member-layer').animate({
         right: '0px'
     }, {
         duration: 500,
         complete: function() {
-            $('.overlay-layer').on('click', function() {
-                $('.ht-member-layer').animate({
-                    right: '-333px'
-                }, {
-                    duration: 500,
-                    complete: function() {
-                        $('.ht-member-layer').remove();
-                        $('.overlay-layer').remove();
-                        $('body').css('overflow', 'auto');
-                    }
+            if (!memberInfo.signedIn) {
+                $('.ht-member-toggle').on('click', function() {
+                    $('.ht-sign-in').toggle();
+                    $('.ht-sign-up').toggle();
                 });
+
+                $('#ht-sign-in').on('click', function() {
+                    signIn();
+                });
+
+                $('#ht-sign-up').on('click', function() {
+                    signUp();
+                });
+            }
+            else {
+                $('#ht-sign-out').on('click', function() {
+                    signOut();
+                });
+            }
+
+            $('.overlay-layer').on('click', function() {
+                closeMemberLayer();
             });
         }
     });
-});
+}
+
+function signIn() {
+    var email = $('#ht-sign-in-email').val().trim();
+    var password = $('#ht-sign-in-password').val().trim();
+    var remember = $('#ht-sign-in-remember').prop('checked');
+
+    if (!email) {
+        alert('이메일을 입력하세요.');
+        $('#ht-sign-in-email').focus();
+        return;
+    }
+    else if (!password) {
+        alert('비밀번호를 입력하세요.');
+        $('#ht-sign-in-password').focus();
+        return;
+    }
+
+    $.ajax({
+        url: '/api/member/signin',
+        method: 'POST',
+        data: {
+            email: email,
+            password: password,
+            remember: remember
+        },
+        success: function(result) {
+            alert(result.email + '님 반갑습니다.');
+            closeMemberLayer();
+        },
+        error: function(jqXHR) {
+            alert(jqXHR.responseJSON.message);
+        }
+    })
+}
+
+function signOut() {
+    $.ajax({
+        url: '/api/member/signout',
+        success: function() {
+            closeMemberLayer();
+        }
+    });
+}
+
+function signUp() {
+    var email = $('#ht-sign-up-email').val().trim();
+    var password = $('#ht-sign-up-password').val().trim();
+    var agree = $('#ht-sign-up-agree').prop('checked');
+
+    if (!email) {
+        alert('이메일을 입력하세요.');
+        $('#ht-sign-up-email').focus();
+        return;
+    }
+    else if (!password) {
+        alert('비밀번호를 입력하세요.');
+        $('#ht-sign-up-password').focus();
+        return;
+    }
+    else if (!agree) {
+        alert('약관에 동의하셔야 합니다.');
+        return;
+    }
+
+    $.ajax({
+        url: '/api/member/signup',
+        method: 'POST',
+        data: {
+            email: email,
+            password: password
+        },
+        success: function(result) {
+            alert('정상적으로 가입되셨습니다.');
+            closeMemberLayer();
+        },
+        error: function(jqXHR) {
+            alert(jqXHR.responseJSON.message);
+        }
+    });
+}
+
+function closeMemberLayer() {
+    $('.ht-member-layer').animate({
+        right: '-333px'
+    }, {
+        duration: 500,
+        complete: function() {
+            $('.ht-member-layer').remove();
+            $('.overlay-layer').remove();
+            $('body').css('overflow', 'auto');
+        }
+    });
+}
